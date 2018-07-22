1075d4b070155764580f72f3a3f59b20ac5182df
seshu <seshsankar17.dl@gmail.com>
2018-07-22 11:33:28 +0000

    proj2 in qa

---
+++ javaapp1/src/main/java/com/qa/proj2/App.java
@@ -0,0 +1,13 @@
+package com.qa.proj2;
+
+/**
+ * Hello proj1 qa!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello proj1 qa!" );
+    }
+}
